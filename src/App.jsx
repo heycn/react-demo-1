a4eceafea92670988f4aeb6b4f3e7d9005e961ee
heycn <x1chennan@qq.com>
2022-03-16 19:17:06 +0800

    在 React 中使用 map

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react"
+import React from 'react'
 
 const App = () => {
-  return(
-    <div>App组件</div>
+  return (
+    <div>
+      App组件
+      <Component numbers={['a', 'b', 'c']} />
+    </div>
   )
 }
+
+const Component = (props) => {
+  return (
+    <div>
+      {props.numbers.map((item, index) => {
+        return (
+          <div>
+            数组第 {index} 项为：{item}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
 export default App
