a15b3e64bbb316e7397fd4db933f2a4cd21a8bf3
heycn <x1chennan@qq.com>
2022-03-17 13:23:04 +0800

    实现 儿子 孙子 n+1

--- src/index.jsx
+++ src/index.jsx
@@ -20,16 +20,30 @@ class Son extends React.Component {
   add() {
     // this.setState.n += 1 为什么不行
     this.setState({ n: this.state.n + 1 })
-    console.log(this.state.n)
   }
   render() {
     return (
       <div className='Son'>
         儿子 n: {this.state.n}
         <button onClick={() => this.add()}>+1</button>
+        <Grandson />
       </div>
     )
   }
+}
+
+const Grandson = () => {
+  // const array = React.useState(0)
+  // const n = array[0]
+  // const setN = array[1]
+  // 下面使用了析构语法优化
+  const [n, setN] = React.useState(0)
+  return (
+    <div>
+      孙子 n: {n}
+      <button onClick={() => setN(n + 1)}>+1</button>
+    </div>
+  )
 }
 
 const rootElement = document.getElementById('root')
