aa7dd08ce021bb3771061c314dcb6f349738de32
heycn <x1chennan@qq.com>
2022-03-21 19:22:39 +0800

    创建 class 的方式

--- src/index.jsx
+++ src/index.jsx
@@ -3,77 +3,25 @@ import ReactDOM from 'react-dom'
 
 import './styles.css'
 
-function App() {
-  return (
-    <div className='App'>
-      爸爸
-      <Son />
-    </div>
-  )
-}
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Son />
+//     </div>
+//   )
+// }
 
 // 类组件
-class Son extends React.Component {
+class App extends React.Component {
   constructor() {
     super()
-    this.state = {
-      n: 0,
-      m: 0,
-      user: {
-        name: 'heycn',
-        age: 18
-      }
-    }
-  }
-  addN() {
-    this.setState(state => {
-      return { n: (state.n += 1) }
-    })
-  }
-  addM() {
-    this.setState(state => {
-      return { m: (state.m += 1) }
-    })
-  }
-  changName() {
-    this.setState({
-      user: {
-        ...this.state.user,
-        name: 'chennan'
-      }
-    })
+    this.state = { n: 0 }
   }
   render() {
     return (
-      <div className='Son'>
-        <div>user.name: {this.state.user.name}</div>
-        <div>user.age: {this.state.user.age}</div>
-        <button onClick={() => this.changName()}>change name</button>
-        <hr />
-        n: {this.state.n}
-        <button onClick={() => this.addN()}>+1</button>
-        m: {this.state.m}
-        <button onClick={() => this.addM()}>+1</button>
-        <Grandson />
-      </div>
+      <div>n: {this.state.n}</div>
     )
   }
-}
-
-// 函数组件
-const Grandson = () => {
-  const [state, setState] = React.useState({
-    n: 0,
-    m: 0
-  })
-  return (
-    <div className='Grandson'>
-      n: {state.n}
-      <button onClick={() => setState({ ...state, n: state.n + 1 })}>+1</button>
-      m: {state.m}
-      <button onClick={() => setState({ ...state, m: state.m + 1 })}>+1</button>
-    </div>
-  )
 }
 
 const rootElement = document.getElementById('root')
