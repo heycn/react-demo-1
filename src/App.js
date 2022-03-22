9ebcbddd0587ad567602ab02ae1c73c2a0e98013
heycn <x1chennan@qq.com>
2022-03-22 13:19:40 +0800

    使用 React.PureComponent 阻止 UI 更新

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class App extends React.Component {
+class App extends React.PureComponent {
   constructor(props) {
     super(props)
     this.state = { n: 1 }
@@ -9,13 +9,6 @@ class App extends React.Component {
     // onClick 函数里对 n 进行了 +1 -1 操作
     this.setState(state => ({ n: state.n + 1 }))
     this.setState(state => ({ n: state.n - 1 }))
-  }
-  shouldComponentUpdate(newProps, newState) {
-    if (newState.n === this.state.n) {
-      return false
-    } else {
-      return true
-    }
   }
   render() {
     console.log('render了')
