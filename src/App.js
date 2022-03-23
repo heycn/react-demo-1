0c2018fa319f557faaa4324766a827609eafaa42
heycn <x1chennan@qq.com>
2022-03-23 16:36:35 +0800

    useState 注意事项：不能局部更新，但是可以使用 ... 来解决

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [n, setN] = useState(0)
+  const [user, setUser] = useState({ name: 'heycn', age: 22 })
+  const onClick = () => {
+    setUser({
+      ...user,
+      name: '名字被改了'
+    })
+  }
   return (
     <div>
-      n: {n}
-      <button onClick={() => setN(n + 1)}>n+1</button>
+      <div>{user.name}</div>
+      <div>{user.age}</div>
+      <button onClick={onClick}>修改名字</button>
     </div>
   )
 }
