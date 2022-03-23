902243327c5f143ccaeefed9078a53beca9551b2
heycn <x1chennan@qq.com>
2022-03-23 11:21:03 +0800

    自定义 Hook 之 useUpdate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
-const App = () => {
+import useUpdate from './useUpdate'
+
+const App = props => {
   const [n, setN] = useState(0)
   const onClick = () => setN(n + 1)
+
+  useUpdate(() => {
+    console.log('变了')
+  }, n)
+
   return (
     <>
-      <div>n: {n}</div>
+      n: {n}
       <button onClick={onClick}>+1</button>
     </>
   )
