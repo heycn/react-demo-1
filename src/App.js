e29f7136e5f62ffdd67d74ad9d0d4ac88486b2ab
heycn <x1chennan@qq.com>
2022-03-23 20:42:10 +0800

    代替 redux 方法

--- src/App.js
+++ src/App.js
@@ -1,30 +1,121 @@
-import { useReducer } from 'react'
+import { createContext, useContext, useEffect, useReducer } from 'react'
 
-const initial = {
-  n: 0
+// 将数据集中在一个 store 对象
+const store = {
+  user: null,
+  books: null,
+  movies: null
 }
 
+// 将所有操作集中在 reducer
 const reducer = (state, action) => {
-  if (action.type === 'add') {
-    return { n: state.n + action.number }
-  } else if (action.type === 'multi') {
-    return { n: state.n * 2 }
-  } else {
-    throw new Error('找不到 type')
+  switch (action.type) {
+    case 'setUser':
+      return { ...state, user: action.user }
+    case 'setBooks':
+      return { ...state, books: action.books }
+    case 'movies':
+      return { ...state, movies: action.movies }
+    default:
+      throw new Error()
   }
 }
+
+// 创建一个 Context
+const Context = createContext(null)
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initial)
-  const onClick = () => {
-    dispatch({ type: 'add', number: 1 })
-  }
+  const [state, dispatch] = useReducer(reducer, store)
+  return (
+    <Context.Provider value={{ state, dispatch }}>
+      <User />
+      <hr />
+      <Books />
+      <hr />
+      <Movies />
+      <hr />
+    </Context.Provider>
+  )
+}
+const User = () => {
+  const { state, dispatch } = useContext(Context)
+  useEffect(() => {
+    ajax('/user').then(user => {
+      dispatch({ type: 'setUser', user })
+    })
+  }, [])
   return (
     <div>
-      n: {state.n}
-      <button onClick={onClick}>+1</button>
+      <h1>个人信息</h1>
+      <div>name: {state.user ? state.user.name : '加载中...'}</div>
     </div>
   )
 }
-
+const Books = () => {
+  const { state, dispatch } = useContext(Context)
+  useEffect(() => {
+    ajax('/books').then(books => {
+      dispatch({ type: 'setBooks', books })
+    })
+  }, [])
+  return (
+    <div>
+      <h1>书籍</h1>
+      <ol>{state.books ? state.books.map(book => <li key={book.id}>{book.name}</li>) : '加载中...'}</ol>
+    </div>
+  )
+}
+const Movies = () => {
+  const { state, dispatch } = useContext(Context)
+  useEffect(() => {
+    ajax('/movies').then(movie => {
+      dispatch({ type: 'setMovies', movie })
+    })
+  }, [])
+  return (
+    <div>
+      <h1>电影</h1>
+      <ol>{state.movies ? state.movies.map(movie => <li key={movie.id}>{movie.name}</li>) : '加载中...'}</ol>
+    </div>
+  )
+}
 export default App
+
+// 帮助函数
+
+// 假 ajax
+// 一秒钟后，根据 path 返回一个对象，必定成功不会失败
+const ajax = path => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (path === '/user') {
+        resolve({
+          id: 1,
+          name: 'heycn'
+        })
+      } else if (path === '/books') {
+        resolve([
+          {
+            id: 1,
+            name: '书籍1'
+          },
+          {
+            id: 2,
+            name: '书籍2'
+          }
+        ])
+      } else if (path === '/movies') {
+        resolve([
+          {
+            id: 1,
+            name: '电影1'
+          },
+          {
+            id: 2,
+            name: '电影2'
+          }
+        ])
+      }
+    }, 1000)
+  })
+}
