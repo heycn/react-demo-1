676328067eec2c7324941aea945ac1ecf9167078
heycn <x1chennan@qq.com>
2022-03-23 11:44:13 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import { useState } from 'react'
 
-import useUpdate from './useUpdate'
-
-const App = props => {
+const App = () => {
   const [n, setN] = useState(0)
-  const onClick = () => setN(n + 1)
-
-  useUpdate(() => {
-    console.log('变了')
-  }, n)
-
   return (
-    <>
+    <div>
       n: {n}
-      <button onClick={onClick}>+1</button>
-    </>
+      <button onClick={() => setN(n + 1)}>n+1</button>
+    </div>
   )
 }
 
